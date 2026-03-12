9d06f10da03e880ead032f68e6cdf2a807e02fb6
Paul Danielse <peg.danielse@gmail.com>
2026-03-12 13:16:32 +0100

    debug and resetup experiment

--- main.py
+++ main.py
@@ -522,43 +522,43 @@ if __name__ == "__main__":
     total_time = 180
     tags_list = (["search_hotel", "recommend"], ["search_hotel", "recommend", "reserve", "user_login"])
 
-    # # -------------------------------
-    # # Baseline + OpenAI/Gemini experiments
-    # # -------------------------------
-    # experiments: list[dict] = []
+    # -------------------------------
+    # Baseline + OpenAI/Gemini experiments
+    # -------------------------------
+    experiments: list[dict] = []
 
-    # # baseline experiments
-    # for tags in tags_list:
-    #     experiments.append(
-    #         {
-    #             "l": f"{start}_baseline_endpoints-{'_'.join(tags)}",
-    #             "t": total_time,
-    #             "tags": tags,
-    #             "baseline": True,
-    #         }
-    #     )
+    # baseline experiments
+    for tags in tags_list:
+        experiments.append(
+            {
+                "l": f"{start}_baseline_endpoints-{'_'.join(tags)}",
+                "t": total_time,
+                "tags": tags,
+                "baseline": True,
+            }
+        )
 
-    # # adaptation experiments with hosted APIs (OpenAI, Gemini)
-    # for tags in tags_list:
-    #     for llm in ("gemini", "openai"):
-    #         for i in (1, 2, 3):
-    #             experiments.append(
-    #                 {
-    #                     "l": f"{start}_{llm}_{i}_endpoints-{'_'.join(tags)}",
-    #                     "t": total_time,
-    #                     "tags": tags,
-    #                     "llm": llm,
-    #                 }
-    #             )
+    # adaptation experiments with hosted APIs (OpenAI, Gemini)
+    for tags in tags_list:
+        for llm in ("gemini", "openai"):
+            for i in (1, 2, 3):
+                experiments.append(
+                    {
+                        "l": f"{start}_{llm}_{i}_endpoints-{'_'.join(tags)}",
+                        "t": total_time,
+                        "tags": tags,
+                        "llm": llm,
+                    }
+                )
 
-    # for experiment in experiments:
-    #     print(f"{experiment['l']}")
+    for experiment in experiments:
+        print(f"{experiment['l']}")
 
-    # print("Starting the experiments in 20 seconds...")
-    # time.sleep(20)
+    print("Starting the experiments in 20 seconds...")
+    time.sleep(20)
 
-    # for experiment in experiments:
-    #     main(argv=[], **experiment)
+    for experiment in experiments:
+        main(argv=[], **experiment)
 
     # -------------------------------
     # vLLM-backed experiments:
